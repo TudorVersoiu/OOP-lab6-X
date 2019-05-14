f9e1584bf34acb7028662976803a427507012b5d
Tudor Versoiu <versoiutudorsorin@gmail.com>
2019-05-14 04:01:08 +0300

    resolve conflicts

--- OOPlab6-7/Controller.cpp
+++ OOPlab6-7/Controller.cpp
@@ -3,36 +3,65 @@
 #include "CarValidator.h"
 
 
+<<<<<<< HEAD
 void Controller::addCar(const std::string & nrInmatriculare, const std::string & producator,
 						const std::string & model,           const std::string & tip) {
 	Car newMasina = Car(nrInmatriculare, producator, model, tip);
     repository.InsertCar(newMasina);
+=======
+void Controller::addCar(
+	const std::string & nr_inmatriculare,
+	const std::string & producator,
+	const std::string & model,
+	const std::string & tip
+	) {
+	Car new_car = Car(nr_inmatriculare, producator, model, tip);
+    
+    repository.InsertCar(new_car);
+>>>>>>> commit from vscode(refactor)
 }
 
-void Controller::removeCar(const std::string & nrInmatriculare) {
-	repository.DeleteCar(nrInmatriculare);
+void Controller::removeCar(const std::string & nr_inmatriculare) {
+	repository.DeleteCar(nr_inmatriculare);
 }
 
+<<<<<<< HEAD
 void Controller::modifyCar(const std::string & nrInmatriculare, const std::string & producator,
                            const std::string & model,           const std::string & tip)
 {
 	Car newCar = Car(nrInmatriculare, producator, model, tip);
 	repository.DeleteCar(nrInmatriculare);
+=======
+void Controller::modifyCar(
+	const std::string & nr_inmatriculare,
+	const std::string & producator,
+    const std::string & model,
+	const std::string & tip
+	) {
+	Car newCar = Car(nr_inmatriculare, producator, model, tip);
+	repository.DeleteCar(nr_inmatriculare);
+>>>>>>> commit from vscode(refactor)
 	repository.InsertCar(newCar);
 }
 
-const std::vector<Car>& Controller::getCarList() const noexcept
-{
+const std::vector<Car>& Controller::getCarList() const noexcept {
 	return repository.getCarList();
 }
 
-std::unique_ptr<Car> Controller::searchCar(const std::string& nrInmatriculare) const
+std::unique_ptr<Car> Controller::searchCar(const std::string& nr_inmatriculare) const
 {
+<<<<<<< HEAD
     auto found_element = std::find_if(repository.getCarList().begin(), repository.getCarList().end(),       // vector range
                          [nrInmatriculare](Car a) { return a.getNRInmatriculare() == nrInmatriculare; });   // lambda function
 
     // If iterator==last, return nullptr, else return *found_element
     return found_element == repository.getCarList().end() ? std::make_unique<Car>(nullptr) : std::make_unique<Car>(*found_element);
+=======
+	for (const auto& masina : repository.getCarList())
+		if (masina.getNRInmatriculare() == nr_inmatriculare)
+			return std::unique_ptr<Car>( std::make_unique<Car>(masina) );
+	return std::unique_ptr<Car>(nullptr);
+>>>>>>> commit from vscode(refactor)
 }
 
 void Controller::getFilteredCars(const std::string& producator, const std::string& tip, std::vector<Car>& output) const
