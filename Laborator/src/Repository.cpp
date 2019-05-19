c123c887479af1c1280d46d035a238342945a47f
Tudor Versoiu <versoiutudorsorin@gmail.com>
2019-05-19 13:19:07 +0300

    repo update

--- Laborator/src/Repository.cpp
+++ Laborator/src/Repository.cpp
@@ -1,32 +1,69 @@
 #include "Repository.h"
+#include <fstream>
 
-void Repository::InsertCar(const Car & masina)
-{
-	if ( findElement(masina.get_registration_number()) != masini.end() )
-		throw
-            repository_exception( std::string("Masina ") + masina.get_registration_number() +
-			std::string(" deja se afla in baza de date!!!") );
-
-	masini.push_back(masina);
+// ============== Basic repository operations ========================
+void _base_repository::base_car_insert(const Car& car_object) {
+		car_collection[car_object.get_registration_number()] = car_object;
+}
+void _base_repository::base_car_delete(const std::string& registration_number) {
+	car_collection.erase(registration_number);
 }
 
-void Repository::DeleteCar(const std::string nrInmatriculare)
-{
-	iterator element = findElement(nrInmatriculare);
-	if (element == masini.end())
-		throw repository_exception("Masina nu se afla in repository!");
-	else
-		masini.erase(element);
+_base_repository::const_iterator _base_repository::find(const std::string& registration_number) const {
+	return car_collection.find(registration_number);
+}
+_base_repository::iterator _base_repository::begin() noexcept {
+	return car_collection.begin();
+}
+_base_repository::iterator _base_repository::end() noexcept {
+	return car_collection.end();
+}
+// ==============
+
+// Normal repository
+void Repository::InsertCar(const Car & car_object) {
+	_base_repository::base_car_insert(car_object);
 }
 
-Repository::iterator Repository::findElement(std::string NrInmatriculare)
-{
-	std::vector<Car>::iterator it;
-	for (it = masini.begin(); it != masini.end(); it++)
-		if (it->get_registration_number() == NrInmatriculare)
-			break;
-
-	return it;
+void Repository::DeleteCar(const std::string& registration_number) {
+	_base_repository::base_car_delete(registration_number);
 }
 
-const std::vector<Car>& Repository::getCarList() const noexcept { return masini; }
+
+// FileRepository
+void FileRepository::save_to_file()
+{
+	std::ofstream output_file(filename);
+	for ( auto car: car_collection ) {
+		output_file
+		<< car.second.get_registration_number() << ","
+		<< car.second.get_producer() << ","
+		<< car.second.get_model() << ","
+		<< car.second.get_type() << '\n';
+	}
+}
+void FileRepository::load_from_file()
+{
+	std::ifstream input_file(filename);
+	while ( !input_file.eof() ) {
+		std::string registration_number,
+					producer,
+					model,
+					type;
+		std::getline(input_file, registration_number, ',');
+		std::getline(input_file, producer, ',');
+		std::getline(input_file, model, ',');
+		std::getline(input_file, type, '\n');
+
+		base_car_insert(Car{registration_number, producer, model, type});
+	}
+}
+
+void FileRepository::InsertCar(const Car& car_object) {
+	_base_repository::base_car_insert(car_object);
+	save_to_file();
+}
+void FileRepository::DeleteCar(const std::string& registration_number) {
+	_base_repository::base_car_delete(registration_number);
+	save_to_file();
+}
